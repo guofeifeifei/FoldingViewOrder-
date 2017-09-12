cf995bd2d4f28929aaa93f34e93f6a5b33c483df
郭飞飞 <myEmail@example.com>
2017-09-12 15:25:25 +0800

    Initial Commit

---
+++ OrderCollection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OrderCollection
+//
+//  Created by ZZCN77 on 2017/9/12.
+//  Copyright © 2017年 ZZCN77. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OrderCollection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OrderCollection
+//
+//  Created by ZZCN77 on 2017/9/12.
+//  Copyright © 2017年 ZZCN77. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
